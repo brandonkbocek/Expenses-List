20d19a936780ad54525225318671bf7eca8dc190
brandonkbocek <brandon.bocek@gmail.com>
2022-02-09 20:44:26 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 function App() {
+
+  const expenses = [
+    {title: 'Car Insurance', amount: 294.51, date: new Date(2022, 1, 1)},
+    {title: 'Football ticket', amount: 394.33, date: new Date(2020, 4, 22)},
+    {title: 'Gameboy', amount: 39.99, date: new Date(2022, 1, 12)},
+    {title: 'Phone bill', amount: 94.22, date: new Date(2021, 3, 29)}
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <NewExpense />
+    <Expenses items={expenses} />
+  </div>
+  
   );
+ 
 }
 
 export default App;
